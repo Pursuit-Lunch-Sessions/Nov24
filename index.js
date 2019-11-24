bec731af500397399c767e507f62ab97ff70b29d
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2019-11-24 13:34:12 -0500

    example complete

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 // Write a function that returns a new array containing all of the strings in the 
 // original array uppercased.
 
-// Input: [{}, 1, "cat", 3, ["hi"], {name: "dog"}, "dog", "bear"]
+let arr =  [{}, 1, "cat", 3, ["hi"], {name: "dog"}, "dog", "bear"]
 // Output: ["CAT", "DOG", "BEAR"]
 
+const upperCaseStrings = (arr) => {
+    let result = [];
+    for(let i = 0; i < arr.length; i++) {
+        if(typeof arr[i] === 'string') {
+            result.push(arr[i].toUpperCase());
+        }
+    }
+    return result;
+}
 
+console.log(upperCaseStrings(arr));
 
 
 
