4ba74311518ffbb2e798f4b384e2988d30164acc
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2019-11-24 12:18:22 -0500

    create problems

---
+++ index.js
@@ -0,0 +1,27 @@
+// Write a function that returns a new array containing all of the strings in the 
+// original array uppercased.
+
+// Input: [{}, 1, "cat", 3, ["hi"], {name: "dog"}, "dog", "bear"]
+// Output: ["CAT", "DOG", "BEAR"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Given an array, return a new array where every non-string element is converted 
+// to an empty string.
+
+// Input: ["a", 123, "b", "c", {name: "cat"}]
+// Output: ["a", "", "b", "c", ""]
